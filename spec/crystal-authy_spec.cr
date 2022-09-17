7df353649b898a60cb92a3dfab19d4811b6f471f
paula4230 <paudepolonia000@yahoo.com>
2022-09-17 20:19:59 +0800

    First Commit

---
+++ spec/crystal-authy_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Crystal::Authy do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/crystal-authy.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Crystal::Authy`
+module Crystal::Authy
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
